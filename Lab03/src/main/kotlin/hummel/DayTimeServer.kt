4a9c18d421e4a685ce0c9f20f90c1818e2e3245c
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-07-30 16:51:30 +0300

    Update DayTimeServer.kt

--- Lab03/src/main/kotlin/hummel/DayTimeServer.kt
+++ Lab03/src/main/kotlin/hummel/DayTimeServer.kt
@@ -4,17 +4,17 @@ import java.net.ServerSocket
 import java.util.*
 
 fun main() {
-    val serverPort = 13
+	val serverPort = 13
 
-    val serverSocket = ServerSocket(serverPort)
-    println("Server is running on port $serverPort")
+	val serverSocket = ServerSocket(serverPort)
+	println("Server is running on port $serverPort")
 
-    val socket = serverSocket.accept()
-    println("Connection from ${socket.inetAddress.hostAddress}")
+	val socket = serverSocket.accept()
+	println("Connection from ${socket.inetAddress.hostAddress}")
 
-    val outputStream = socket.getOutputStream()
-    val date = Date().toString() + "\n"
-    outputStream.write(date.toByteArray())
+	val outputStream = socket.getOutputStream()
+	val date = Date().toString() + "\n"
+	outputStream.write(date.toByteArray())
 
-    socket.close()
+	socket.close()
 }
