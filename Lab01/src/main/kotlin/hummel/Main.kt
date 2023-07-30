381bfaa789deccabe5487ad1ffe38412518712a9
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-07-30 16:50:18 +0300

    Update Main.kt

--- Lab01/src/main/kotlin/hummel/Main.kt
+++ Lab01/src/main/kotlin/hummel/Main.kt
@@ -4,20 +4,20 @@ import java.net.NetworkInterface
 import java.net.SocketException
 
 fun main() {
-    try {
-        val interfaces = NetworkInterface.getNetworkInterfaces()
-        while (interfaces.hasMoreElements()) {
-            val networkInterface = interfaces.nextElement()
-            val mac = networkInterface.hardwareAddress
-            if (mac != null) {
-                print("MAC Address of " + networkInterface.name + ": ")
-                for (i in mac.indices) {
-                    System.out.format("%02X%s", mac[i], if (i < mac.size - 1) "-" else "")
-                }
-                println()
-            }
-        }
-    } catch (e: SocketException) {
-        e.printStackTrace()
-    }
+	try {
+		val interfaces = NetworkInterface.getNetworkInterfaces()
+		while (interfaces.hasMoreElements()) {
+			val networkInterface = interfaces.nextElement()
+			val mac = networkInterface.hardwareAddress
+			if (mac != null) {
+				print("MAC Address of " + networkInterface.name + ": ")
+				for (i in mac.indices) {
+					System.out.format("%02X%s", mac[i], if (i < mac.size - 1) "-" else "")
+				}
+				println()
+			}
+		}
+	} catch (e: SocketException) {
+		e.printStackTrace()
+	}
 }
