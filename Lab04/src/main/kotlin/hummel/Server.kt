b887b5fff0f78d855f9ba21d26149d435e455619
Hummel009 <hummel.turbamentum@gmail.com>
2023-09-01 15:54:34 +0300

    fix

--- Lab04/src/main/kotlin/hummel/Server.kt
+++ Lab04/src/main/kotlin/hummel/Server.kt
@@ -2,7 +2,6 @@ package hummel
 
 import java.io.IOException
 import java.net.ServerSocket
-import kotlin.system.exitProcess
 
 fun main() {
 	Server()
@@ -10,21 +9,15 @@ fun main() {
 
 class Server {
 	private val controlPort = 1025
-	private var welcomeSocket: ServerSocket? = null
+	private var welcomeSocket = ServerSocket(controlPort)
 	private var serverRunning = true
 
 	init {
-		try {
-			welcomeSocket = ServerSocket(controlPort)
-		} catch (e: IOException) {
-			println("Could not create server socket")
-			exitProcess(-1)
-		}
 		println("FTP Server started listening on port $controlPort")
 		var noOfThreads = 0
 		while (serverRunning) {
 			try {
-				val client = welcomeSocket!!.accept()
+				val client = welcomeSocket.accept()
 				val dataPort = controlPort + noOfThreads + 1
 
 				val w = Worker(client, dataPort)
@@ -36,12 +29,6 @@ class Server {
 				e.printStackTrace()
 			}
 		}
-		try {
-			welcomeSocket!!.close()
-			println("Server was stopped")
-		} catch (e: IOException) {
-			println("Problem stopping server")
-			exitProcess(-1)
-		}
+		welcomeSocket.close()
 	}
 }
