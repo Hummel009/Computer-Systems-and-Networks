fc0a307f75b421d8aecebbbf24e9d9499f7554be
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-11-17 22:35:12 +0300

    Update build.gradle.kts

--- Lab03/build.gradle.kts
+++ Lab03/build.gradle.kts
@@ -3,9 +3,10 @@ import java.time.format.DateTimeFormatter
 
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.9.20"
+	id("application")
 }
 
-group = "org.example"
+group = "hummel"
 version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 
 repositories {
@@ -15,13 +16,27 @@ repositories {
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+application {
+	mainClass = "hummel.MainKt"
 }
 
 tasks {
 	jar {
 		manifest {
-			attributes(mapOf("Main-Class" to "hummel.MainKt"))
+			attributes(
+				mapOf(
+					"Main-Class" to "hummel.MainKt"
+				)
+			)
 		}
 		from(embed.map {
 			if (it.isDirectory) it else zipTree(it)
