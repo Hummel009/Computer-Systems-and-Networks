d05dc174dfaacc2c150abaae99839ffee72469a8
Hummel009 <hummel.turbamentum@gmail.com>
2023-11-23 15:07:13 +0300

    works

---
+++ Lab04/src/main/kotlin/hummel/Main.kt
@@ -0,0 +1,28 @@
+package hummel
+
+import java.io.IOException
+import java.net.ServerSocket
+
+const val controlPort: Int = 1025
+var welcomeSocket: ServerSocket = ServerSocket(controlPort)
+var serverRunning: Boolean = true
+
+fun main() {
+	println("FTP Server started listening on port $controlPort")
+	var noOfThreads = 0
+	while (serverRunning) {
+		try {
+			val client = welcomeSocket.accept()
+			val dataPort = controlPort + noOfThreads + 1
+
+			val w = Worker(client, dataPort)
+			println("New connection received. Worker was created.")
+			noOfThreads++
+			w.start()
+		} catch (e: IOException) {
+			println("Exception encountered on accept")
+			e.printStackTrace()
+		}
+	}
+	welcomeSocket.close()
+}
