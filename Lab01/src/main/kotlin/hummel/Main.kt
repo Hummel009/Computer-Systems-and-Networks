ac119a0ae75c4aa35914ec1d0c7a7567aa1bc9b4
Hummel009 <hummel.turbamentum@gmail.com>
2023-12-01 15:27:04 +0300

    Update Main.kt

--- Lab01/src/main/kotlin/hummel/Main.kt
+++ Lab01/src/main/kotlin/hummel/Main.kt
@@ -1,23 +1,18 @@
 package hummel
 
 import java.net.NetworkInterface
-import java.net.SocketException
 
 fun main() {
-	try {
-		val interfaces = NetworkInterface.getNetworkInterfaces()
-		while (interfaces.hasMoreElements()) {
-			val networkInterface = interfaces.nextElement()
-			val mac = networkInterface.hardwareAddress
-			if (mac != null) {
-				print("MAC Address of " + networkInterface.name + ": ")
-				for (i in mac.indices) {
-					System.out.format("%02X%s", mac[i], if (i < mac.size - 1) "-" else "")
-				}
-				println()
+	val interfaces = NetworkInterface.getNetworkInterfaces()
+	while (interfaces.hasMoreElements()) {
+		val networkInterface = interfaces.nextElement()
+		val mac = networkInterface.hardwareAddress
+		mac?.let {
+			print("MAC Address of " + networkInterface.name + ": ")
+			for (i in it.indices) {
+				System.out.format("%02X%s", it[i], if (i < it.size - 1) "-" else "")
 			}
+			println()
 		}
-	} catch (e: SocketException) {
-		e.printStackTrace()
 	}
 }
