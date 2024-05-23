0ab8c70551eac797a57b2195fd6278b6620e1679
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-23 12:40:08 +0300

    k2

--- appLab4/build.gradle.kts
+++ appLab4/build.gradle.kts
@@ -12,7 +12,7 @@ version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
 }
 
 java {
